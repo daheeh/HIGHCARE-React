1c659e0810546012a011e38fb38a8f6a82334d03
kimng1003 <kimnagyeong50@gmail.com>
2023-08-12 13:43:26 +0900

    first commit-React

---
+++ src/approval/apvSmmitbarCopy.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ApvSummitBar() {
+	return (
+	<div className="apv-topbar">
+		<div>
+			<button class="summit">제출</button>
+			<button>파일첨부</button>
+			<button>결재선</button>
+			<button>설정</button>
+		</div>
+	</div>
+	);
+}
+
+export default ApvSummitBar;
+
