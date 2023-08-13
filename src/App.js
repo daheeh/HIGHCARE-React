1e13fd11f09ba39fd00bbc556e653096958d19db
HDhye <hdhye89@gmail.com>
2023-08-13 21:56:27 +0900

    충돌삭제

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-
 import EmpMain from './empMain';
-<<<<<<< HEAD
-import './App.css';
-import { Route, Router, Routes } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import RouteApv from './js/approval/routeApv';
 import './App.css';
->>>>>>> 4c460d820d6ced41c090b4c967bfc219be3b6b1c
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <div className="">
-        <header className="">
-          
-          <EmpMain />
-        </header>
-        <Routes>
-          <Route path="/*" element={<EmpMain />} />
-        </Routes>
-      </div>
-    </Router>
-
-
-=======
       <Routes>
         <Route path="/" element={<EmpMain />} />
       </Routes>
     </Router>,
     <RouteApv />
->>>>>>> 4c460d820d6ced41c090b4c967bfc219be3b6b1c
   );
 }
 
