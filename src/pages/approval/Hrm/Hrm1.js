63ea38cb26fbe329368b42a5e02ef026457ce75d
kimng1003 <kimnagyeong50@gmail.com>
2023-08-24 10:38:08 +0900

    #7 연차 신청서 초안 완성. api연결 예정

--- src/pages/approval/Hrm/Hrm1.js
+++ src/pages/approval/Hrm/Hrm1.js
@@ -28,9 +28,9 @@ function Hrm1() {
 			endDate: '',
 			type: '연차',
 			comment: '',
-			amount: '',
-			offType1: '',
-			offType2: '',
+			amount: 0,
+			offtype1: '',
+			offtype2: '',
 		},
 		lastClickedRadio: '',
 	});
@@ -44,29 +44,30 @@ function Hrm1() {
 			},
 		}));
 	};
-
 	const [dateRange, setDateRange] = useState("");
 
 	useEffect(() => {
 		if (formData.apvVacation.startDate && formData.apvVacation.endDate) {
-			// const formattedStartDate = new Date(formData.apvVacation.startDate);
-			// const formattedEndDate = new Date(formData.apvVacation.endDate);
 			const formattedStartDate = new Date(formData.apvVacation.startDate).toLocaleDateString();
 			const formattedEndDate = new Date(formData.apvVacation.endDate).toLocaleDateString();
 			setDateRange(`${formattedStartDate} ~ ${formattedEndDate}`);
 		}
 	}, [formData.apvVacation.startDate, formData.apvVacation.endDate]);
-
+	
 	const [usedDays, setUsedDays] = useState(0);
-
+	
 	useEffect(() => {
 		if (formData.apvVacation.startDate && formData.apvVacation.endDate) {
+	
 			let startDate = new Date(formData.apvVacation.startDate);
 			let endDate = new Date(formData.apvVacation.endDate);
+			let offtype1 = formData.apvVacation.offtype1;
+			let offtype2 = formData.apvVacation.offtype2;
 	
 			if (endDate < startDate) {
-				alert("종료일자는 시작일자보다 빠를 수 없습니다.");
+				alert("종료일자는 시작일자보다 빠를 수 없습니다");
 				endDate = startDate;
+				offtype2 = offtype1;
 				setFormData((prevFormData) => ({
 					...prevFormData,
 					apvVacation: {
@@ -82,140 +83,158 @@ function Hrm1() {
 	
 			let totalDays = yearDiff * 365 + monthDiff * 30 + dayDiff + 1;
 	
-			let adjustedUsedDays = totalDays;
-	
-			console.log("formData.apvVacation.offType1 : ", formData.apvVacation.offType1);
-			if (formData.apvVacation.offType1 === "09:00" || formData.apvVacation.offType1 === "14:00") {
-				adjustedUsedDays -= 0.5;
-				console.log(adjustedUsedDays);
+			if (offtype1 !== '') {
+				totalDays -= 0.5;
 			}
 	
-			if (formData.apvVacation.offType2 === "09:00" || formData.apvVacation.offType2 === "14:00") {
-				adjustedUsedDays -= 0.5;
+			if (offtype2 !== '') {
+				totalDays -= 0.5;
 			}
-			console.log("adjustedUsedDays : ", adjustedUsedDays);
-			setUsedDays(adjustedUsedDays);
-		}
-	}, [formData.apvVacation.startDate, formData.apvVacation.endDate, formData.apvVacation.offType1, formData.apvVacation.offType2]);
 	
-
-
-	const onChangeHandler = (e) => {
-		const { name, value } = e.target;
-		if (name === 'offtype1') {
-			const time = e.target.checked ? value : '';
 			setFormData((prevFormData) => ({
 				...prevFormData,
 				apvVacation: {
 					...prevFormData.apvVacation,
-					startDate: `${prevFormData.apvVacation.startDate.split(' ')[0]} ${time}`,
+					amount: totalDays,
 				},
 			}));
 		}
+	}, [formData.apvVacation.startDate, formData.apvVacation.endDate, formData.apvVacation.offtype1, formData.apvVacation.offtype2]);
+	
 
-		if (name === 'offtype2') {
-			const time = e.target.checked ? value : '';
-			setFormData((prevFormData) => ({
-				...prevFormData,
-				apvVacation: {
-					...prevFormData.apvVacation,
-					endDate: `${prevFormData.apvVacation.endDate.split(' ')[0]} ${time}`,
-				},
-			}));
-		}
-	};
 
-	const handleSubmission = () => {
-		const convertedStartDate = new Date(formData.apvVacation.startDate).getTime();
-		const convertedEndDate = new Date(formData.apvVacation.endDate).getTime();
 
-		const formDataWithTimestamps = {
-			...formData,
+const onChangeHandler = (e) => {
+	const { name, value } = e.target;
+	if (name === 'offtype1') {
+		const time = e.target.checked ? value : '';
+		setFormData((prevFormData) => ({
+			...prevFormData,
 			apvVacation: {
-				...formData.apvVacation,
-				startDate: convertedStartDate,
-				endDate: convertedEndDate,
+				...prevFormData.apvVacation,
+				startDate: `${prevFormData.apvVacation.startDate.split(' ')[0]} ${time}`,
+				offtype1: value,
 			},
-		};
+		}));
+	}
 
-		dispatch(callApvExp1API({ formData: formDataWithTimestamps }));
+	if (name === 'offtype2') {
+		const time = e.target.checked ? value : '';
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			apvVacation: {
+				...prevFormData.apvVacation,
+				endDate: `${prevFormData.apvVacation.endDate.split(' ')[0]} ${time}`,
+				offtype2: value,
+			},
+		}));
+	}
+};
+
+const handleSubmission = () => {
+	const convertedStartDate = new Date(formData.apvVacation.startDate).getTime();
+	const convertedEndDate = new Date(formData.apvVacation.endDate).getTime();
+
+	const formDataWithTimestamps = {
+		...formData,
+		apvVacation: {
+			...formData.apvVacation,
+			startDate: convertedStartDate,
+			endDate: convertedEndDate,
+		},
 	};
 
+	dispatch(callApvExp1API({ formData: formDataWithTimestamps }));
+};
 
-	const updateIsUrgency = (newIsUrgency) => {
-		setFormData(prevFormData => ({
-			...prevFormData,
-			isUrgency: newIsUrgency
-		}));
-	};
+const onCommentChangeHandler = (e) => {
+	const { value } = e.target;
+	setFormData((prevFormData) => ({
+		...prevFormData,
+		apvVacation: {
+			...prevFormData.apvVacation,
+			comment: value === ''? '연차 사용': value,
+		},
+	}));
+};
 
-	useEffect(() => {
-		const currentDate = new Date();
-		setFormData(prevFormData => ({
-			...prevFormData,
-			writeDate: currentDate
-		}));
-	}, []);
+const updateIsUrgency = (newIsUrgency) => {
+	setFormData(prevFormData => ({
+		...prevFormData,
+		isUrgency: newIsUrgency
+	}));
+};
 
-	console.log('formData.apvVacation : ', formData.apvVacation);
-	return (
-		<section>
-			<ApvMenu />
-			<div>
-				<ApvSummitBar onsubmit={handleSubmission} updateIsUrgency={updateIsUrgency} />
-				<div className="containerApv">
-					<div className="apvApvTitle">연차신청서</div>
-					<ApvSummitLine />
-					<div className="apvContent">
-						<div className="apvContentHrm1">
-							<div className="column1">휴가 종류</div>
-							<div className="column2">연차</div>
+useEffect(() => {
+	const currentDate = new Date();
+	setFormData(prevFormData => ({
+		...prevFormData,
+		writeDate: currentDate
+	}));
+}, []);
+
+console.log('formData.apvVacation : ', formData.apvVacation);
+return (
+	<section>
+		<ApvMenu />
+		<div>
+			<ApvSummitBar onsubmit={handleSubmission} updateIsUrgency={updateIsUrgency} />
+			<div className="containerApv">
+				<div className="apvApvTitle">연차신청서</div>
+				<ApvSummitLine />
+				<div className="apvContent">
+					<div className="apvContentHrm1">
+						<div className="column1">휴가 종류</div>
+						<div className="column2">연차</div>
+					</div>
+					<div className="apvContentHrm1">
+						<div className="column1">시작일자</div>
+						<div className="column2">
+							<input className="input1" type="date" placeholder="날짜 입력"
+								name="apvVacation.startDate"
+								value={formData.apvVacation.startDate.split(' ')[0]}
+								onChange={(e) => onSelectDateHandler(e.target.value, 'startDate')}
+							/>
+							<label className='labelName'> (반차) </label>
+							<label><input type="radio" name="offtype1" value="09:00" onChange={onChangeHandler} />오전</label>
+							<label><input type="radio" name="offtype1" value="14:00" onChange={onChangeHandler} />오후</label>
+							<label className='labelName'> - 미선택 시 전일 - </label>
 						</div>
-						<div className="apvContentHrm1">
-							<div className="column1">시작일자</div>
-							<div className="column2">
-								<input className="input1" type="date" placeholder="날짜 입력"
-									name="apvVacation.startDate"
-									value={formData.apvVacation.startDate.split(' ')[0]}
-									onChange={(e) => onSelectDateHandler(e.target.value, 'startDate')}
-								/>
-								<label className='labelName'> (반차) </label>
-								<label><input type="radio" name="offtype1" value="09:00" onChange={onChangeHandler} />오전</label>
-								<label><input type="radio" name="offtype1" value="14:00" onChange={onChangeHandler} />오후</label>
-								<label className='labelName'> - 미선택 시 전일 - </label>
-							</div>
+					</div>
+					<div className="apvContentHrm1">
+						<div className="column1">종료일자</div>
+						<div className="column2">
+							<input className="input1" type="date" placeholder="날짜 입력"
+								name="apvVacation.endDate"
+								value={formData.apvVacation.endDate.split(' ')[0]}
+								onChange={(e) => onSelectDateHandler(e.target.value, 'endDate')}
+							/>
+							<label className='labelName'> (반차) </label>
+							<label><input type="radio" name="offtype2" value="09:00" onChange={onChangeHandler} />오전</label>
+							<label><input type="radio" name="offtype2" value="14:00" onChange={onChangeHandler} />오후</label>
+							<label className='labelName'> - 미선택 시 전일 - </label>
 						</div>
-						<div className="apvContentHrm1">
-							<div className="column1">종료일자</div>
-							<div className="column2">
-								<input className="input1" type="date" placeholder="날짜 입력"
-									name="apvVacation.endDate"
-									value={formData.apvVacation.endDate.split(' ')[0]}
-									onChange={(e) => onSelectDateHandler(e.target.value, 'endDate')}
-								/>
-								<label className='labelName'> (반차) </label>
-								<label><input type="radio" name="offtype2" value="09:00" onChange={onChangeHandler} />오전</label>
-								<label><input type="radio" name="offtype2" value="14:00" onChange={onChangeHandler} />오후</label>
-								<label className='labelName'> - 미선택 시 전일 - </label>
-							</div>
-						</div>
-						<div className="apvContentHrm1">
-							<div className="column1">기간</div>
-							<div className="column2">{dateRange}</div>
-						</div>
-						<div className="apvContentHrm1">
-							<div className="column1">연차 사용 개수</div>
-							<div className="column2">{usedDays < 0 ? 0 : usedDays} 개</div>
-						</div>
-						<div className="apvContentDetail">-사유-</div>
-						<div className="apvContentDetailComent">
-							<textarea placeholder="연차사용" rows="20" onChange={onChangeHandler}></textarea>
-						</div>
+					</div>
+					<div className="apvContentHrm1">
+						<div className="column1">기간</div>
+						<div className="column2">{dateRange}</div>
+					</div>
+					<div className="apvContentHrm1">
+						<div className="column1">연차 사용 개수</div>
+						<div className="column2">{formData.apvVacation.amount} 개</div>
+					</div>
+					<div className="apvContentDetail">- 사유 -</div>
+					<div className="apvContentDetailComent">
+						<textarea placeholder="3일 이상인 경우만 작성" rows="20"
+						value={formData.apvVacation.comment}
+						onChange={onCommentChangeHandler}
+						onBlur={onCommentChangeHandler}></textarea>
 					</div>
 				</div>
 			</div>
-		</section>
-	);
+		</div>
+	</section>
+);
 }
 
 export default Hrm1;
