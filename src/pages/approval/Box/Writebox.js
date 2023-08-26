33da7126ad497232e706c6fd803a445bb236dd85
kimng1003 <kimnagyeong50@gmail.com>
2023-08-26 15:35:44 +0900

    #5 결재함 페이징 적용

--- src/pages/approval/Box/Writebox.js
+++ src/pages/approval/Box/Writebox.js
@@ -19,6 +19,8 @@ function WriteBox() {
 	const results = useSelector(state => state.approval);
     const [selectedStatus, setSelectedStatus] = useState('결재진행중');
 
+    const totalPages = Math.ceil(results.length / itemsPerPage);
+
     useEffect(() => {
         dispatch(callApvWriteBoxAPI({ empNo, apvStatus: '결재진행중'}));
         console.log('results : ', results);
@@ -28,6 +30,11 @@ function WriteBox() {
         dispatch(callApvWriteBoxAPI({ empNo, apvStatus }));
         setSelectedStatus(apvStatus);
     };
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
 
     return (
 
@@ -77,13 +84,25 @@ function WriteBox() {
                             }
                         </tbody>
                     </table>
-                    <div className="paging">
+                    {/* <div className="paging">
                         <span>1</span>
                         <span>2</span>
                         <span>3</span>
                         <span>4</span>
                         <span>5</span>
-                    </div>
+                    </div> */}
+                    <div className="paging">
+    {Array.from({ length: totalPages }, (_, index) => (
+        <span
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+        >
+            {index + 1}
+        </span>
+    ))}
+</div>
+
                 </div>
             </div>
         </section>
