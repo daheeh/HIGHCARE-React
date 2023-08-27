62b32fcdae546ac2f3c0cfe22880d64b1fec411b
kimng1003 <kimnagyeong50@gmail.com>
2023-08-27 13:29:26 +0900

    #3 리액트-전자결재 메인페이지 (인사 리스트 드래그드랍 적용)

--- src/pages/approval/Hrm/HrmList.js
+++ src/pages/approval/Hrm/HrmList.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 function HrmList() {
 
-    const handleDragStart = (e, link) => {
-        e.dataTransfer.setData('link', link);
+
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+    const [list, setList] = useState([
+        'hrm1',
+        'hrm2',
+        'hrm3',
+        'hrm4',
+        'hrm5',
+        'hrm6',
+        'hrm7',
+    ]);
+
+    const linkTextMapping = {
+        hrm1: '연차신청서',
+        hrm2: '기타휴가신청서',
+        hrm3: '서류발급신청서',
+        hrm4: '시말서xx',
+        hrm5: '연장근무신청서',
+        hrm6: '사직서xx',
+        hrm7: '기안서',
+
     };
     
-    
+
+
+    const dragStart = (e, position) => {
+        dragItem.current = position;
+        e.target.style.backgroundColor = '#FFC338';
+    }
+
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position;
+        
+        
+    }
+        
+    const drop = (e) => {
+        e.target.style.backgroundColor = '#f4f4f4';
+        const newList = [...list];
+        const dragItemValue = newList[dragItem.current];
+        newList.splice(dragItem.current, 1);
+        newList.splice(dragOverItem.current, 0, dragItemValue);
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setList(newList);
+    };
+
+
     return (
         <div className="apvMainListRList">
             <div className="listTitle">인사</div>
-{/* 
-            <div draggable="true" data-link="Hrm1"
-                onDragStart={(e) => handleDragStart(e, "Hrm1")}>
-            연차신청서
-            </div>
-            <div draggable="true" data-link="Hrm2">기타휴가신청서</div>
-            <div draggable="true" data-link="Hrm3">서류발급신청서</div>
-            <div draggable="true" data-link="Hrm4">시말서xx</div>
-            <div draggable="true" data-link="Hrm5">연장근무신청서</div>
-            <div draggable="true" data-link="Hrm6">사직서xx</div>
-            <div draggable="true" data-link="Hrm7">기안서</div> */}
 
-            <div><Link to="Hrm1">연차신청서</Link></div>
-            <div><Link to="Hrm2">기타휴가신청서</Link></div>
-            <div><Link to="Hrm3">서류발급신청서</Link></div>
-            <div><Link to="Hrm4">시말서xx</Link></div>
-            <div><Link to="Hrm5">연장근무신청서</Link></div>
-            <div><Link to="Hrm6">사직서xx</Link></div>
-            <div><Link to="Hrm7">기안서</Link></div>
+            { list.map((item, idx) => (
+                <div>
+                <Link to={`/${item}`} key={idx}>
+                    <div
+                        draggable
+                        onDragStart={(e) => dragStart(e, idx)}
+                        onDragEnter={(e) => dragEnter(e, idx)}
+                        onDragEnd={drop}
+                        onDragOver={(e) => e.preventDefault()}
+                    >
+                        {linkTextMapping[item]}
+                    </div>
+                </Link>
+                </div>
+            ))}
         </div>
 
     );
