3e21e90a68b23197e49408d7c7df24588ddfdf5e
AHRIM <belieb3764@gmail.com>
2023-09-01 16:09:26 +0900

    #16 출근등록

--- src/apis/PmAPICalls.js
+++ src/apis/PmAPICalls.js
@@ -1,6 +1,9 @@
 import {
     GET_MEMBER,
     GET_TREEVIEW_ONE,
+    GET_MANAGEMENT,
+    POST_PM_MANAGEMENT,
+    POST_PM_END
 } from '../modules/TreeModule';
 import {
     GET_TREEVIEW_TWO
@@ -97,5 +100,110 @@ export const callTreeviewTwoAPI = () => {
         }
         };
     };
+
+export const callManagementAPI = () => {
+        console.log('[PmAPICalls] callManagementAPI Call');
+        
+        const requestURL = `http://localhost:8080/api/pm/management`;
+        
+        return async (dispatch, getState) => {
+        
+            try{
+                const result = await fetch(requestURL, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "*/*"
+                    }
+                })
+                .then(response => {
+                    console.log('-----------------> \n', response);
+                    return response.json()
+                });
+        
+                console.log('[PmAPICalls] callManagementAPI RESULT :>>>>>>>>>>>>>>>> ', result);
+        
+                dispatch({type:  GET_MANAGEMENT, payload: result});
+            } catch(error) {
+                console.error('[PmAPICalls] Error in callManagementAPI: ', error);
+            }
+            };
+        };
+
+export const callMgStartAPI = ({ formData }) => {
+    console.log('[PmAPICalls] callMgStartAPI Call');
+    
+    const requestURL = `http://localhost:8080/api/pm/management/insert`;
+    
+    return async (dispatch, getState) => {
+
+        console.log('[PmAPICalls] callMgStartAPI formData : ', formData);
+        try{
+            const result = await fetch(requestURL, {
+                method: "POST",
+                headers: {
+                    "Accept": "*/*",
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify(formData),
+            })
+            // .then(response => {
+            //     console.log('-----------------> \n', response);
+            //     return response.json()
+            // });
+            .then(response => response.json());
+
+            console.log('[ApprovalAPICalls] callMgEndtAPI RESULT : ', result);
+
+            dispatch({ type: POST_PM_MANAGEMENT, payload: result });
+            return result;
+
+        } catch (error) {
+            console.error('[ApprovalAPICalls] Error in callMgEndtAPI: ', error);
+            throw error;
+        }
+
+    };
+};
+
+export const callMgEndAPI = () => {
+    console.log('[PmAPICalls] callMgEndAPI Call');
+    
+    const requestURL = `http://localhost:8080/api/pm/management/update`;
+    
+    return async (dispatch, getState) => {
+
+        console.log('[PmAPICalls] callMgEndAPI formData : ', );
+        try{
+            const result = await fetch(requestURL, {
+                method: "POST",
+                headers: {
+                    "Accept": "*/*",
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify(),
+            })
+            // .then(response => {
+            //     console.log('-----------------> \n', response);
+            //     return response.json()
+            // });
+            .then(response => response.json());
+
+            console.log('[ApprovalAPICalls] callMgEndAPI RESULT : ', result);
+
+            dispatch({ type: POST_PM_END, payload: result });
+            return result;
+
+        } catch (error) {
+            console.error('[ApprovalAPICalls] Error in callMgEndAPI: ', error);
+            throw error;
+        }
+
+    };
+};
+
+
 
 
