a7a52e83d55cc184499f7f52ab4347384ec30244
HEO\uwill <gjdbdlfsla147@gmail.com>
2023-09-12 15:39:50 +0900

    no message

--- src/layouts/MainLayOut.js
+++ src/layouts/MainLayOut.js
@@ -28,14 +28,10 @@ function MainLayOut() {
 
                 <div className={MainLayOutCss.box}>
                     <h2>시설예약</h2>
-<<<<<<< HEAD
-                    <p>테스트</p>
-                    <a href="#" style={{color:'gray'}}>더 보기</a>
-=======
+
                     <br/><br/>
                     <Link to="/reservation">예약하러 가기</Link>
 
->>>>>>> heo
                 </div>
             </div>
         </>
