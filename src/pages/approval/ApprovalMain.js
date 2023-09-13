916707bd490d639bd4f1d0349b35183320538205
kimng1003 <kimnagyeong50@gmail.com>
2023-09-13 18:03:13 +0900

    css수정

--- src/pages/approval/ApprovalMain.js
+++ src/pages/approval/ApprovalMain.js
@@ -10,21 +10,21 @@ import './Approval.css';
 
 function ApvMain() {
 
-    // const authes = useSelector(state => state.authes);
-    // const empNo = authes.empNo;
-    // console.log("ApvMain empNo : ", empNo);
+    const authes = useSelector(state => state.authes);
+    const empNo = authes.empNo;
+    console.log("ApvMain empNo : ", empNo);
 
-    // const dispatch = useDispatch();
-    // const approvalMain = useSelector(state => state.approval);
+    const dispatch = useDispatch();
+    const approvalMain = useSelector(state => state.approval);
 
-    // const [counts, setCounts] = useState({
-    //     countTodayInProgress: 0,
-    //     countTodayUrgency: 0,
-    //     countInProgress: 0,
-    //     countUrgency: 0,
-    //     countNewReceive: 0,
-    //     countRejected: 0,
-    // });
+    const [counts, setCounts] = useState({
+        countTodayInProgress: 0,
+        countTodayUrgency: 0,
+        countInProgress: 0,
+        countUrgency: 0,
+        countNewReceive: 0,
+        countRejected: 0,
+    });
 
     // useEffect(() => {
     //     async function fetchcounts() {
@@ -61,7 +61,7 @@ function ApvMain() {
                 <div className="apvMainList">
                     <div className="apvMainListMy">
                         <div className="apvMainListLTitle">최근 결재 양식</div>
-                        <ApvMyList show={false}/>
+                        <ApvMyList/>
                     </div>
                     <div className="apvMainListList">
                         <div className="apvMainListRTitle">탬플릿 양식</div>
